5fc373cc74ff532f4172042393938673aafd7f49
projectperfect <jprfct@protonmail.com>
2022-09-16 23:24:48 -0500

    previous button added

--- app.js
+++ app.js
@@ -75,4 +75,14 @@ nextBtn.addEventListener('click', function(){
     currentItem = 0;
   }
   showPerson(currentItem);
+});
+
+// show prev person
+
+prevBtn.addEventListener('click', function(){
+  currentItem--;
+  if(currentItem < 0){
+    currentItem = reviews.length - 1;
+  }
+  showPerson(currentItem);
 })
